b8b23baa5a019a3f9eb972ad4517729fc48f397c
Shira Feldinger <shirafeldinger@gmail.com>
2020-08-30 12:56:47 +0300

    Updates

---
+++ precache-manifest.7470f0d568082e3d059cad281120fc0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904917c50ac579c92629bcc6ea26d121",
+    "url": "/animal-store/index.html"
+  },
+  {
+    "revision": "130bffd33e4a8e22d93b",
+    "url": "/animal-store/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e57c137542520e4a9598",
+    "url": "/animal-store/static/js/2.0f53ff7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-store/static/js/2.0f53ff7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130bffd33e4a8e22d93b",
+    "url": "/animal-store/static/js/main.1c5f504f.chunk.js"
+  },
+  {
+    "revision": "01a3dcd01f2f5ac4fd91",
+    "url": "/animal-store/static/js/runtime-main.817dde36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animal-store/static/media/logo.5d5d9eef.svg"
+  }
+]);
