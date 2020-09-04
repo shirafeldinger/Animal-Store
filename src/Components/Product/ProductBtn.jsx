f5140ca02fc296424744f10fb9fd941a64807381
Shira Feldinger <shirafeldinger@gmail.com>
2020-09-04 19:45:38 +0300

    'new'

--- src/Components/Product/ProductBtn.jsx
+++ src/Components/Product/ProductBtn.jsx
@@ -7,6 +7,15 @@ const ProductBtn = styled.button`
   transform: translateY(-50%);
   transform: translateX(-50%);
   margin: 10px;
+  background-color: #fc6f38;
+  background-img: "https://cdn0.iconfinder.com/data/icons/shopping-cart-26/1000/Shopping-Basket-03-512.png";
+  border: 2px solid #fc6f38;
+  color: white;
+
+  &:hover {
+    background-color: #c96c48;
+    border: 2px solid #c96c48;
+  }
 `;
 
 export default ProductBtn;
