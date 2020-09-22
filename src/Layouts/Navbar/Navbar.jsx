ed8458e85a32de8707c743fdd3a150f949c29076
Shira Feldinger <shirafeldinger@gmail.com>
2020-09-22 04:23:12 +0300

    style navbar

--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -4,26 +4,25 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   const [isOpen, setOpen] = useState(false);
   return (
-    <nav className={isOpen ? "navbar  responsive" : "navbar"}>
+    <nav className="navbar">
       {" "}
       <img
         className="shop-img"
         src="https://halfpi.nl/wp-content/uploads/2018/02/shop-icon-300x300.png"
         alt="shop"
       />
-      <div>
-        {" "}
-        <img
-          className="nav-icon"
-          onClick={() => {
-            setOpen(!isOpen);
-          }}
-          src="https://img.icons8.com/ios-filled/50/000000/menu.png"
-        />
+      <img
+        className="nav-icon"
+        onClick={() => {
+          setOpen(!isOpen);
+        }}
+        src="https://img.icons8.com/ios-filled/50/000000/menu.png"
+      />
+      <div className={isOpen ? "responsive" : ""}>
+        <Link to="/">דף הבית</Link> <Link to="/cart">עגלת קניות</Link>
+        <Link to="/register">התחבר / הרשמה</Link>
+        <Link to="/contact">צור קשר</Link>
       </div>
-      <Link to="/">דף הבית</Link> <Link to="/cart">עגלת קניות</Link>
-      <Link to="/register">התחבר / הרשמה</Link>
-      <Link to="/contact">צור קשר</Link>
     </nav>
   );
 }
